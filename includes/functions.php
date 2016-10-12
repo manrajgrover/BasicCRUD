b8a02c3355f68d874b5b8794439f7d5270120a2a
Manraj Singh <manrajsinghgrover@gmail.com>
2016-10-12 21:26:17 +0530

    Functions file commented

--- includes/functions.php
+++ includes/functions.php
@@ -1,20 +1,46 @@
 <?php
+
+  /**
+   * Checks if name is valid
+   * @param  String  $name Employee Name
+   * @return boolean       If name is valid
+   */
   function isValidName($name) {
     return preg_match("/^[a-zA-Z ]+$/", $name) && strlen($name) < 50;
   }
 
+  /**
+   * Check if email is valid
+   * @param  String  $email Employee email
+   * @return boolean        If employee is valid
+   */
   function isValidEmail($email) {
     return filter_var($email, FILTER_VALIDATE_EMAIL) && strlen($email) < 50;
   }
 
+  /**
+   * Check if contact is valid
+   * @param  String  $contact Employee contact
+   * @return boolean          If contact is valid
+   */
   function isValidContact($contact) {
     return preg_match("/^(0|\+91)?[789]\d{9}$/", $contact) && strlen($contact) < 15;
   }
 
+  /**
+   * Check if designation is valid
+   * @param  String  $designation Employee designation
+   * @return boolean              If designation is valid
+   */
   function isValidDesignation($designation) {
     return !empty($designation) && is_string($designation);
   }
 
+  /**
+   * Check if all given fields are valid or not
+   * @param  Array  $data  Employee details
+   * @return Array         Any errors, if yes then their details
+   */
   function isValid($data) {
     $arrResponse = array('error' => false, 'message' => array());
     if (!isValidName($data['name'])) {
